7b6dad7198e17da539fe5e5b75311fede4180d3d
Chen Xin <xin.chen@taodinet.com>
2014-11-19 16:38:09 +0800

    update env

--- priv/cowboy_rack.rb
+++ priv/cowboy_rack.rb
@@ -9,7 +9,7 @@ Dir.chdir(ARGV[0])
 
 require 'erlectricity'
 require 'stringio'
-
+require 'rack'
 module Rack
   module Handler
     class CowboyRack
@@ -18,6 +18,19 @@ module Rack
         receive do |f|
           f.when(:env, Object) do |env|
             env = env.inject({}){|env_hash, kv| env_hash[kv[0]] = kv[1].to_s; env_hash } 
+              # body_len = INPUT.read(4).unpack("N")[0]
+              # INPUT = IO.new(3)
+              # rack_input = StringIO.new(INPUT.read(body_len))
+              # rack_input.set_encoding(Encoding::BINARY) if rack_input.respond_to?(:set_encoding)
+
+            env.update({"rack.version" => Rack::VERSION,
+               # "rack.input" => rack_input,
+               "rack.errors" => $stderr,
+               "rack.multithread" => false,
+               "rack.multiprocess" => false,
+               "rack.run_once" => false,
+               "rack.url_scheme" => ["yes", "on", "1"].include?(ENV["HTTPS"]) ? "https" : "http"
+             })
             status, headers, body = app.call(env)
             # f.send!(:response, env.inspect)
             f.send!(:response, [
@@ -41,3 +54,4 @@ module Rack
     end
   end
 end
+  
